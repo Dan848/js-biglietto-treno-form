d4e706c11776714e789927b7d86670d215e357bf
Dan848 <daniele.ceravolo15@gmail.com>
2023-03-09 19:11:23 +0100

    add script

--- js/script.js
+++ js/script.js
@@ -10,3 +10,47 @@ L'output del prezzo finale va messo fuori in forma umana (con massimo due decima
 
 const questionKm = document.getElementById("km");
 const questionAge = document.getElementById("age");
+
+
+const btnCalc = document.getElementById("btnCalc");
+
+btnCalc.addEventListener ("click", function(){
+    let km = questionKm.value;
+    let age = questionAge.value;
+    const priceKm = 0.21;
+    const priceUnderage = 0.8;
+    const priceOverage = 0.6;
+    let finalPrice;
+    const printTicket = document.querySelector(".card-info");
+    
+    if (age<18) {
+        finalPrice = Math.round(((km * priceKm) * priceUnderage) * 100) / 100;
+
+        printTicket.innerHTML = `<ul class="fw-bold">
+        <li class="list-group-item">Chilometri indicati: ${km}</li>
+        <li class="list-group-item">Età Passeggero: ${age}</li>
+        <li class="list-group-item">Sconto applicato: Minorenne (-20%)</li>
+        <li class="list-group-item text-uppercase text-success h5">Prezzo finale: €${finalPrice}</li>
+        </ul>`;
+    }
+    else if (age>65) {
+        finalPrice = Math.round(((km * priceKm) * priceOverage) * 100) / 100;
+
+        printTicket.innerHTML = `<ul class="fw-bold">
+        <li class="list-group-item">Chilometri indicati: ${km}</li>
+        <li class="list-group-item">Età Passeggero: ${age}</li>
+        <li class="list-group-item">Sconto applicato: Over 65</li>
+        <li class="list-group-item text-uppercase text-success h5">Prezzo finale: €${finalPrice}</li>
+        </ul>`;
+    }
+    else {
+        finalPrice = Math.round((km * priceKm) * 100) / 100;
+
+        printTicket.innerHTML = `<ul class="fw-bold">
+        <li class="list-group-item">Chilometri indicati: ${km}</li>
+        <li class="list-group-item">Età Passeggero: ${age}</li>
+        <li class="list-group-item">Sconto applicato: Nessuno</li>
+        <li class="list-group-item text-uppercase text-success h5">Prezzo finale: €${finalPrice}</li>
+        </ul>`;
+    }
+})
